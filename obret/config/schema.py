44ba6e8899096cd2e25bfd577598a30a4ac34fe2
ittk1229 <s2010485@u.tsukuba.ac.jp>
2025-01-25 18:23:32 +0900

    add: トークナイザにmecabを用いたindexingの処理

--- obret/config/schema.py
+++ obret/config/schema.py
@@ -7,5 +7,5 @@ class BaseConfig(BaseSettings):
     vault_dirpath: Path
     vault_dict_filepath: Path
     index_dirpath: str
-    stopwords_filepath: Path
+    stopword_filepath: Path
     exclude_dirnames: list[str]
